c2a5fc403a592bbb9e852c3921f30fae7d25764b
ZhuChongLin <8210856@qq.com>
2024-04-23 22:35:02 +0800

    完善HTTP响应处理

--- src/EventHandle.php
+++ src/EventHandle.php
@@ -18,33 +18,42 @@ namespace ViSwoole\HttpServer;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use Throwable;
+use ViSwoole\Core\Facades\App;
+use ViSwoole\Core\Facades\Server;
+use ViSwoole\HttpServer\Contract\ResponseInterface;
+use ViSwoole\HttpServer\Router\RouteMiss;
 
 class EventHandle
 {
   public static function onRequest(
     Request  $request,
     Response $response
-  )
+  ): bool
   {
-    if ($request->getMethod() === 'OPTIONS') {
-      $response->header = [
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Allow-Headers' => '*',
-        'Access-Control-Allow-Methods' => '*',
-      ];
-      return $response->end();
-    }
     try {
       $psr7Request = \ViSwoole\HttpServer\Request::create($request);
       $psr7Response = \ViSwoole\HttpServer\Response::create($response);
+      // 匹配路由
+      $route = Router::collector()->matchRoute($psr7Request);
+      if ($route instanceof RouteMiss) {
+        $handle = $route->handler;
+        $middleware = [];
+      } else {
+        $handle = $route['handler'];
+        $middleware = $route['middleware'];
+      }
+      $result = Middleware::process($handle, $middleware);
+      if ($result instanceof ResponseInterface) {
+        return $result->send();
+      } elseif (is_array($result) | is_object($result)) {
+        // 返回的不是response对象 则对返回的参数进行json格式化。
+        return $psr7Response->json($result)->send();
+      } else {
+        return $psr7Response->send((string)$result);
+      }
     } catch (Throwable $e) {
-
+      $exceptionHandle = Server::getConfig()['exception_handle'];
+      return App::invokeMethod([$exceptionHandle, 'render'], [$e]);
     }
-    return true;
-  }
-
-  public static function onStart()
-  {
-
   }
 }
